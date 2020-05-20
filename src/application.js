a2ae3cd7fc1f87c1eb5c1dbc02c605092cb9b867
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-05-20 15:08:38 -0700

    reset database

--- src/application.js
+++ src/application.js
@@ -41,7 +41,7 @@ module.exports = function application(
   app.use("/api", appointments(db, actions.updateAppointment));
   app.use("/api", interviewers(db));
 
-  if (ENV === "development" || ENV === "test") {
+  //if (ENV === "development" || ENV === "test") {
     Promise.all([
       read(path.resolve(__dirname, `db/schema/create.sql`)),
       read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
@@ -59,7 +59,7 @@ module.exports = function application(
       .catch(error => {
         console.log(`Error setting up the reset route: ${error}`);
       });
-  }
+  //}
 
   app.close = function() {
     return db.end();
